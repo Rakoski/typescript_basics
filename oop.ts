63170fb3d30ff9bd8d1349f25cf55b72c087c5c8
Rakoski <mastrakoski@gmail.com>
2023-11-14 05:43:12 -0300

    oop in typescript

---
+++ oop.ts
@@ -0,0 +1,21 @@
+class Pessoa {
+    nome: string;
+    idade: number;
+    altura: number;
+
+    constructor(nome: string, idade: number, altura: number) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    comer() {
+        return "comendo..."
+    }
+}
+
+const alturaDaPessoa = 1.80
+const idadeDaPessoa = 18
+const nomeDaPessoa = "Lucas"
+const pessoa1 = new Pessoa(nomeDaPessoa, idadeDaPessoa, alturaDaPessoa);
+console.log(pessoa1, pessoa1.comer())
